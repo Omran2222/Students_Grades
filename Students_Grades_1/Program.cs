ca6de55c0fcaf4b9833ee63cbdbb5e6b6bdfc4dc
Crydman <45429281+Omran2222@users.noreply.github.com>
2021-09-01 22:44:40 +0300

    Students Grades

---
+++ Students_Grades_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Students_Grades_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string strYN; 
+            do
+            {
+                Console.WriteLine("Enter Student Name");
+                Console.ReadLine();
+                Console.WriteLine("Enter Student Degree");
+                double Degree = double.Parse(Console.ReadLine());
+
+                if (Degree >= 60)
+                {
+                    Console.WriteLine("Passed");
+                }
+                else
+                {
+                    Console.WriteLine("Failed");
+
+                }
+                Console.WriteLine("=======================================================");
+                Console.WriteLine("DO you want to add another student Y/N: ");
+                strYN = Console.ReadLine();
+            }
+            while (strYN == "y" || strYN == "Y");
+            
+
+
+        }           
+
+        
+    }
+}
